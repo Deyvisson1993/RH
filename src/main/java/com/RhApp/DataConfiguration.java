f09fa6fe902ad0999e6ea431968a36b01d23732d
Deyvisson Mendes <deyvisson.mendes@condominiolpbv.local>
2023-08-04 09:58:08 -0300

    first commit

---
+++ src/main/java/com/RhApp/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.RhApp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/apprh?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("Daraujo93@");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter adapter() {
+
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+
+	}
+
+}
